9341657ac0485d47fd49c108d1dda98f5a1e811d
ilnar <your.email@example.com>
2013-09-13 19:14:33 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BulatApp::Application.config.secret_token = '19036146f287542a7822e1045c3cc5b1916e843d83adb31ad84cce51c9620fd32dd330450b482545b847bae278fef4a8a5fc5cd54e14ebbef911d87169501242'
