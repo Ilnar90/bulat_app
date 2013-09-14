a5caf08ad90b7bfc47f9022b5cad71d2d74c17d2
ilnar <your.email@example.com>
2013-09-14 16:46:04 +0400

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BulatApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
